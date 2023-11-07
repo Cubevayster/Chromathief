f1b9a35a78904e99f4360c577b78090b48c6495b
Adele <56159423+Adeleimpa@users.noreply.github.com>
2023-11-07 15:31:46 +0100

    Pause Menu
    Press 'ESCAPE' to try it
    Ressource: https://www.youtube.com/watch?v=JivuXdrIHK0

---
+++ Chromathief/Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // normal rate
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true); // display pause menu
+        Time.timeScale = 0f; // freeze game
+        gameIsPaused = true;
+
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("restarting game...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f; // normal speed
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitting game...");
+        Application.Quit();
+    } 
+}
