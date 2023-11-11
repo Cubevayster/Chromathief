60b1180ed7b140f87a2110a9e99e59b0d6bb2bdc
Houle Adrien <adrienhoule01@gmail.com>
2023-11-11 17:58:39 +0100

    fix victory scene

--- Chromathief/Assets/Scripts/HUD/Exit.cs
+++ Chromathief/Assets/Scripts/HUD/Exit.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI score;
+    [SerializeField] TextMeshProUGUI temp;
     private void OnTriggerEnter(Collider collider)
     {
         PlayerControler controller = collider.GetComponent<PlayerControler>();
+      
+
 
         if(controller != null)
         {
             if (controller.FoundExitKey)
             {
+                CustomManager.Instance.ScoreText = score.text;
+                CustomManager.Instance.TimeText = temp.text;
                 SceneManager.LoadScene("VictoryScene");
             }
         }
